de51403a757806b09307bb492850194494e1f55e
Oqueber Navarro <oqueber@gmail.com>
2024-11-15 12:19:15 +0100

    BMO ASSISTANT V1

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,55 @@
-/*
- ESP8266 - Ejemplo de uso de pantalla OLED 128x64 monocroma por Dani No www.esploradores.com
-
- Este ejemplo utiliza el núcleo de la librería gráfica de Adafruit GFX y 
- la librería SSD1306 específica para pantallas monocromas 128x64 y 128x32.
- Estas librerías deben estar instaladas para su funcionamiento.
- Para instalarlas se debe seleccionar:
-   -Programa -> Incluir librería -> Gestionar Librerías -> Adafruit GFX Library
-   -Programa -> Incluir librería -> Gestionar Librerías -> Adafruit SSD1306
-
- Cuando se instala la librería SSD1306 está configurada por defecto para pantallas 128x32.
- Al utilizarla para una pantalla 128x64 es preciso reconfigurarla.
- Para hacerlo debe abrir el fichero Adafruit_SSD1306.h con un editor y 
- descomentar la línea 69 (// #define SSD1306_128_64  --->  #define SSD1306_128_64) y 
- comentar la línea 70 (#define SSD1306_128_32  --->  //#define SSD1306_128_32).
- Así mismo se debe verificar en la línea 51 que la dirección de I2C de la pantalla es correcta 
- (#define SSD1306_I2C_ADDRESS   0x3C  // 128x64 0x3D (default) or 0x3C (if SA0 is grounded))
-
-Por último, también se debe verificar que en la línea 108 de este sketch
-la dirección I2C de la pantalla también es correcta.
+/**
+ * @file    main.c
+ * @brief   Development project with ESP8266.
+ *
+ * @author  Oqueber Navarro
+ * @date    15-11/2024
+ *
+ * @details This project involves the development of an application for the ESP8266 microcontroller 
+ * using the Arduino programming language. It is compiled and designed to be executed 
+ * in PlatformIO with Visual Studio Code.
+ * 
+ * For more information, visit: 
+ * @see     https://oqueber.es/bmo
  */
 
+// Standart lib
 #include "Arduino.h"
-#include "pitches.h"
-
 #include <Wire.h>
-#include <Adafruit_GFX.h>      //Nucleo de la librería gráfica.
-#include <Adafruit_SSD1306.h>  //Librería para pantallas OLED monocromas de 128x64 y 128x32
+
+// Display libraries
+#include <Adafruit_GFX.h>      
+#include <Adafruit_SSD1306.h>  
 #include <Fonts/FreeMonoBoldOblique12pt7b.h>
 
-// The ESP8266 pin connected to piezo buzzer
+typedef enum
+{
+	DISPLAY_FACE_DISPABLE = 0,
+	DISPLAY_FACE_ANGRY,
+	DISPLAY_FACE_LOOKING,
+	DISPLAY_FACE_NERVOUS
+}face_animation_t;
+
+// The ESP8266 pin connected to buzzer
 #define BUZZZER_PIN   15
 // The ESP8266 pin connected to buttom.
-#define BUTTOM_PIN    10
+#define BUTTON_PIN    10
 
-int button_status = 0;
-int play_sound_select = 0;
+//ISRs need to have IRAM_ATTR before the function definition to run the interrupt code in RAM.
+IRAM_ATTR void ISR_button_pressed(void);
 
-#define OLED_RESET -1 
-Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
+// Flag button to receive irq
+volatile bool flag_animation_stop = false;
+volatile bool flag_btn_enable = false;
+// Allow switching between animations
+int index_select_animation = DISPLAY_FACE_ANGRY;
+
+// Create Display object
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+//
+// BUZZER
+// 
 
 // notes in the melody:
 void play_sound_alarm()
@@ -111,22 +123,12 @@ void play_sound()
   }
 }
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
 
-  // initialize Buzzer pin as an output.
-  pinMode(BUZZZER_PIN, OUTPUT);
+//
+// Display
+//
 
-  // Initialize Buttom pin as a input
-  pinMode(BUTTOM_PIN, INPUT);
-
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // //Inicializa pantalla con en la dirección 0x3D para la conexión I2C.
-  display.clearDisplay();
-}
-
-void face_brave(void)
+void face_angry(void)
 {
   display.fillScreen(0);         //Limpiamos la pantalla
   
@@ -138,7 +140,11 @@ void face_brave(void)
     display.fillTriangle(0,0, display.width()-1,0, 45 , i, 0);
     
     //Refrescamos la pantalla para visualizarlos
-    display.display();  
+    display.display(); 
+
+    // If flag animation stop is enable -> stop
+    if(flag_animation_stop){return;}
+
     delay(30);           
   }
 
@@ -151,13 +157,16 @@ void face_brave(void)
     display.fillTriangle(0,0, display.width()-1,0, 45 , i, 0);
     
     //Refrescamos la pantalla para visualizarlos
-    display.display();  
+    display.display(); 
+
+    // If flag animation stop is enable -> stop
+    if(flag_animation_stop){return;}
+
     delay(20);           
   }
 }
 
-
-void face_nervius(void)
+void face_nervous(void)
 {
   display.fillScreen(0);         //Limpiamos la pantalla
   
@@ -173,6 +182,10 @@ void face_nervius(void)
     
     //Refrescamos la pantalla para visualizarlos
     display.display();  
+
+    // If flag animation stop is enable -> stop
+    if(flag_animation_stop){return;}
+
     delay(30);           
   }
 
@@ -188,7 +201,11 @@ void face_nervius(void)
                           0);
     
     //Refrescamos la pantalla para visualizarlos
-    display.display();  
+    display.display();
+    
+    // If flag animation stop is enable -> stop
+    if(flag_animation_stop){return;}
+
     delay(20);           
   }
 }
@@ -205,7 +222,11 @@ void face_looking(void)
     display.fillRoundRect(i + 55,17,30,40,15,1);
     
     //Refrescamos la pantalla para visualizarlos
-    display.display();  
+    display.display();
+
+    // If flag animation stop is enable -> stop
+    if(flag_animation_stop){return;}
+
     delay(30);           
   }
 
@@ -220,69 +241,160 @@ void face_looking(void)
     display.fillRoundRect(i + 55,17,30,40,15,1);
     
     //Refrescamos la pantalla para visualizarlos
-    display.display();  
+    display.display();
+
+    // If flag animation stop is enable -> stop
+    if(flag_animation_stop){return;}
+
     delay(20);           
   }
 }
 
+void face_disable(void)
+{
+    //Limpiamos la pantalla
+    display.fillScreen(0);         
+    
+    //Refrescamos la pantalla para visualizarlos
+    display.display();
+}
+
+
+//
+// BUTTOM SEQUENCE
+//
+
+void btn_detect_press(void)
+{
+	int num_btn_press = 0;
+        
+	// Check if it is a short or long press
+	while(digitalRead(BUTTON_PIN) == 0)
+	{
+		num_btn_press++;
+		delay(100);
+
+		// Detected long pressed button
+		if( num_btn_press >= 10)
+		{
+			break;
+		}
+	}
+
+
+	// SHORT PULSE
+	if ( num_btn_press < 10)
+	{
+		index_select_animation++;
+		play_sound_success();
+	}
+	// LONG PULSES
+	else
+	{	
+		// DISABLE ANIMATION
+		if (index_select_animation != DISPLAY_FACE_DISPABLE)
+		{
+			index_select_animation = DISPLAY_FACE_DISPABLE;
+			play_sound_error();
+		}
+		// ENABLE ANIMATION
+		else
+		{
+			index_select_animation = DISPLAY_FACE_ANGRY;
+			play_sound_success();
+		}
+	}
+
+	// Enable button
+	attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), ISR_button_pressed, FALLING );
+	// Enable animation 
+	flag_animation_stop = false;
+	// Clean flag btn 
+	flag_btn_enable = false;
+}
+
+//
+// SETUP
+//
+
+void setup()
+{
+    // initialize LED digital pin as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    // initialize Buzzer pin as an output.
+    pinMode(BUZZZER_PIN, OUTPUT);
+
+    // Initialize Buttom pin as a input
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), ISR_button_pressed, FALLING);
+
+    // INIT and Clean DISPLAY 128x64
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.clearDisplay();
+}
+
 void loop()
 {
-
-  // WAIT TO PRESS BUTTOM
-  if(digitalRead(BUTTOM_PIN) != button_status)
-  {
-    // ONLY WHEN BUTTOM IS PRESSING
-    if(digitalRead(BUTTOM_PIN) == 0)
+    // SELECT Animation
+    switch (index_select_animation)
     {
-      play_sound_select++;
-    }
-    
-    // KEEP THE CHANGE
-    button_status = digitalRead(BUTTOM_PIN);
-  }
+        case DISPLAY_FACE_DISPABLE:
+        {
+            face_disable();
+            delay(500);
+            break;
+        }
+        case DISPLAY_FACE_ANGRY:
+        {
+            face_angry();
+            break;
+        }
 
-  // SELECT SOUND
-  switch (play_sound_select)
-  {
-    case 0:
-    {
-      play_sound_success();
-      break;
+        case DISPLAY_FACE_LOOKING:
+        {
+            face_looking();
+            break;
+        }
+		case DISPLAY_FACE_NERVOUS:
+        {
+			face_nervous();
+            break;
+        }
+        default:
+            index_select_animation = DISPLAY_FACE_ANGRY;
+        break;
     }
-    case 1:
-    {
-      play_sound_error();
-      break;
-    }
-    case 2:
-    {
-      play_sound_alarm();
-      break;
-    }
-    default:
-      play_sound_select = 0;
-      break;
-  }
 
-  //PUNTOS EN LAS CUATRO ESQUINAS DE LA PANTALLA
-  //display.fillScreen(0);         //Limpiamos la pantalla
-  
-  //display.drawCircle(32,32,10,1);
-  //display.drawCircle(64,32,10,1);
-  //display.fillCircle(80,40,20,1);
-  //display.fillCircle(32,40,20,1);
-  //display.drawRoundRect(0,0,127,31,10,1);
-  
-  //face_brave();
-  //face_nervius();
-  face_looking();
 
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a second
-  delay(500);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-   // wait for a second
-  delay(500);
+    //if an interrup has occured
+    if (flag_btn_enable)
+    {
+		// Check if it is really pressed
+        if(digitalRead(BUTTON_PIN) == 0)
+		{
+			btn_detect_press();
+		}
+    }
+
+    // Toggle the led
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+//
+// IRQ
+//
+
+IRAM_ATTR void ISR_button_pressed(void) 
+{
+    // BTN IS PRESSING
+    if (flag_btn_enable == false)
+    {
+		// Set btn flag to enable
+		flag_btn_enable = true;
+		// Stop all animation 
+		flag_animation_stop = true;
+		// Disable btn
+		detachInterrupt( digitalPinToInterrupt(BUTTON_PIN) );
+    }
 }
